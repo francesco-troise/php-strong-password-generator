fdfc1b0db5a4e5a5da949cb7e415108d5f1c6089
francesco-troise <francesco.troise98@gmail.com>
2026-02-27 13:55:12 +0100

    MILESTONE 4

--- functions/function.php
+++ functions/function.php
@@ -1,14 +1,45 @@
-<?php 
+<?php
 
-    function psw_generator($psw_length, $Allowed_chars){
-    //Accepts the password length and the allowed characters for generation
-        $password = "";
-        for($i=0; $i <  $psw_length; $i++){
-            $index_chars = random_int(0 , strlen($Allowed_chars) - 1 );
-            //Generation of a random number to use as a random index to obtain characters from $Allowed_chars
-            $password .= $Allowed_chars[$index_chars];
-        };
-        return $password;
+function psw_generator($psw_length, $Allowed_chars, $flags_array)
+//Accepts the password length and the allowed characters for generation
+{
+    $name_keys = array_keys($flags_array);
+    $repeat = true;
+    for ($i = 0; $i < count($flags_array); $i++) {
+        if ($name_keys[$i] == "flag_repeat" && $flags_array["flag_repeat"] == 0) {
+            $repeat = false;
+            //Flag_repeat
+        } else if ($name_keys[$i] == "flag_chars" && $flags_array["flag_chars"] == 0) {
+            $Allowed_chars = preg_replace("/[a-zA-Z]/", "", $Allowed_chars);
+            //If flag_chars  is disabled, so $Allowed_chars lost his chars
+
+        } else if ($name_keys[$i] == "flag_num" && $flags_array["flag_num"] == 0) {
+            $Allowed_chars = preg_replace("/[0-9]/", "", $Allowed_chars);
+            //If flag_num is disabled, so $Allowed_chars lost his numbers
+        } else if ($name_keys[$i] == "flag_symbol" && $flags_array["flag_symbol"] == 0) {
+            $Allowed_chars = preg_replace("/[^a-zA-Z0-9]/", "", $Allowed_chars);
+        }
     }
 
-?>
+
+    $cont = 0;
+    $password = "";
+    while ($cont < $psw_length) {
+        if ($repeat == true) {
+            $index_chars = random_int(0, strlen($Allowed_chars) - 1);
+            //Generation of a random number to use as a random index to obtain characters from $Allowed_chars
+            $password .= $Allowed_chars[$index_chars];
+            $cont++;
+        } else {
+            $index_chars = random_int(0, strlen($Allowed_chars) - 1);
+            if (str_contains($password, $Allowed_chars[$index_chars])) {
+                continue;
+            } else {
+                $password .= $Allowed_chars[$index_chars];
+                $cont++;
+            }
+        }
+    }
+
+    return $password;
+}
--- index.php
+++ index.php
@@ -1,42 +1,85 @@
-<?php 
-    session_start();
-    //session start
+<?php
+session_start();
+//session start
+require './functions/function.php';
 
-    require"./functions/function.php";
-    $Allowed_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()';
-    //Allowed characters for password generation
+$Allowed_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()';
+//Allowed characters for password generation
 
-    if(isset($_GET["psw_length"])){
+
+if (isset($_GET['psw_length'])) {
     //On loading: generate password only if the length parameter is present
-        $psw_length = $_GET["psw_length"];
-        $password = psw_generator($psw_length, $Allowed_chars);
-        $_SESSION["password"] = $password;
+    $psw_length = $_GET['psw_length'];
 
-        header("Location: ./show_password.php");
+    $flag_repeat = isset($_GET['psw_repeat']) ? (int) $_GET['psw_repeat'] : 0;
+    $flag_chars = isset($_GET['psw_chars']) ? (int) $_GET['psw_chars'] : 0;
+    $flag_upper_lower = (isset($_GET['upper_case']) || isset($_GET['lower_case'])) ? 1 : 0;
+    $flag_num = isset($_GET['psw_numbers']) ? (int) $_GET['psw_numbers'] : 0;
+    $flag_symbol = isset($_GET['psw_symbols']) ? (int) $_GET['psw_symbols'] : 0;
+    //Flags representing password parameters
 
-        exit;
+    $flags_array = ['psw_length' => $psw_length, 'flag_repeat' => $flag_repeat, 'flag_chars' => $flag_chars, 'flag_upper_lower' => $flag_upper_lower, 'flag_num' => $flag_num, 'flag_symbol' => $flag_symbol];
+    foreach ($flags_array as $key => $value) {
+        // Saltiamo il controllo sulla lunghezza, perché non è una flag 0/1
+        if ($key !== 'psw_length') {
+            if (!in_array($value, [0, 1, '0', '1'])) {
+                header('Location: ./index.php');
+                exit();
+            }
+        }
     }
+
+    $password = psw_generator($psw_length, $Allowed_chars, $flags_array);
+    //Function that generates password - uses password length and allowed characters
+    $_SESSION['password'] = $password;
+
+    header('Location: ./show_password.php');
+    //redirect to display page as soon as the password is generated
+
+    exit();
+}
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form-password</title>
 </head>
+
 <body>
     <header>
-        
+
     </header>
     <main>
         <h1>FORM DI GENERAZIONE PASSWORD</h1>
         <p>Decidi la lunghezza della tua password!</p>
         <form action="./index.php" method="GET">
+
             <label for="form_password">Scegli una lunghezza</label>
             <input type="number" name="psw_length" id="form_passowrd" min="4" max="500" required>
+
+            <span class="info_radio" for="psw_repeat_yes">Consenti ripetizioni di caratteri</span>
+
+            <label for="psw_repeat_yes">Si</label>
+            <input type="radio" name="psw_repeat" id="psw_repeat_yes" value="1" checked>
+
+            <label for="psw_repeat_no">No</label>
+            <input type="radio" name="psw_repeat" id="psw_repeat_no" value="0">
+
+            <label for="psw_chars">Lettere</label>
+            <input type="checkbox" id="psw_chars" name="psw_chars" value="1">
+
+            <label for="psw_numbers">Numeri</label>
+            <input type="checkbox" name="psw_numbers" id="psw_numbers" value="1">
+
+            <label for="psw_symbols">Simboli</label>
+            <input type="checkbox" name="psw_symbols" id="psw_symbols" value="1">
+
             <button type="submit">Invia!</button>
             <a href="./index.php">Ripulisci il risultato</a>
         </form>
@@ -44,7 +87,7 @@
         <p>Sarai indirizzato alla pagina di visualizzazione</p>
     </main>
     <footer></footer>
-    
-</body>
-</html>
 
+</body>
+
+</html>
